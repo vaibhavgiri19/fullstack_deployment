f3adc05e9bf2affc6f0eea4a7700d77cae1400a7
Vaibhav Giri <your-email@example.com>
2026-04-25 09:43:04 +0530

    update

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const API = axios.create({
- baseURL:"http://localhost:5000"
+ baseURL:"https://fullstack-deployment-lrtf.onrender.com"
 })
 
 API.interceptors.request.use((req)=>{
